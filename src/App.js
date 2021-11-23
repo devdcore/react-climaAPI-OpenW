0e65dc94bd3a4e24ccf5a48238e378f49967769c
Daniel Valle <danielvalle25@gmail.com>
2021-11-23 10:39:25 -0400

    Project
    Proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
+
 
 function App() {
+
+    // state busqueda del formulario
+    const [busqueda, guardarbusqueda] = useState({
+      ciudad: '',
+      pais: ''
+    });
+
+    // Se usa para pasarlo al componente Formulario y al pasarlo a useEffect no se ejecuta con cada change del teclado
+    const [consultar, guardarConsultar] = useState(false); 
+    const [resultado, guardarResultado] = useState({});
+    const [error, guardarError] = useState(false);
+
+    const { ciudad, pais } = busqueda;
+
+    
+  useEffect(() => {
+
+    const consultarAPI = async () => {
+      if(consultar) {        
+      const appId = '97e1333c78023a19100a10654a0952a5';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarResultado(resultado);
+      guardarConsultar(false);
+      // Verifica una respuesta correcta
+      if(resultado.cod === '404'){
+        guardarError(true);
+      }else {
+        guardarError(false);
+      }
+      }      
+    }
+
+    consultarAPI();
+
+    // eslint-disable-next-line
+  }, [consultar]);
+
+  let componente;
+  if(error) {
+    componente = <Error mensaje="No hay resultados"/>
+  }else {
+    componente = <Clima
+    resultado={resultado}
+    />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header
+      titulo="Clima Reac App">
+      </Header>
+
+      <div className="contenedor-form">
+          <div className="container">
+              <div className="row">
+                <div className="col m6 s12">
+                  <Formulario
+                  busqueda={busqueda}
+                  guardarbusqueda={guardarbusqueda}
+                  guardarConsultar={guardarConsultar}
+                  />
+                </div>
+                <div className="col m6 s12">
+                  {componente}
+                </div>
+              </div>
+          </div>
+      </div>
+    </Fragment>
   );
 }
 
